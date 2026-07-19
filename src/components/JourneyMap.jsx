e2b1700a93af6a008cdec511d564923c97fee2b1
Ilyar <elyar@smail.nju.edu.cn>
2026-07-19 22:24:52 +0800

    Fix: switch to OSM tiles + compress images for better loading

--- src/components/JourneyMap.jsx
+++ src/components/JourneyMap.jsx
@@ -144,8 +144,9 @@ export default function JourneyMap() {
             zoomControl={true}
           >
             <TileLayer
-              attribution=""
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
+              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+              className="dark-map-tiles"
             />
             <MapAnimator />
             <RouteLine />
